678a2e4e5a90bfaf03a43c55a7061298bd8c701a
Stephen J. Lu <Stephen@StephenJLu.com>
2024-12-12 00:02:54 -0800

    Refactor header component to improve role handling and update config structure

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,29 +1,28 @@
 import React from "react";
-import { TextAnim, TextFade, DecoderText } from "../Components";
+import { TextFade, DecoderText } from "../Components";
 import styles from "./header.module.css";
 import config from "../../config.json";
 
 export const Header = () => {    
   const fadeText = config.name;
-  const codeText = config.roles;
-  const delay = config.delay;
-  const asciiText = config.ascii;
+  const roles = config.roles;
+  const baseDelay = config.delay;  
 
   return (
     <header>      
       <div className={styles.header} data-bs-theme="dark">
         <div className={styles.headerBackground} />
         <h1>
-          <TextFade fadeText={fadeText} delay={delay}/>
+          <TextFade fadeText={fadeText} delay={baseDelay}/>
         </h1>
         <span className={styles.subtitle}>
-         {codeText.map((text, index) => (
+          {roles.map((role, index) => (
             <React.Fragment key={index}>
-              <DecoderText text={`${text}`} delay={delay} />
+              <DecoderText text={role.role} delay={role.delay} />
               <br />
             </React.Fragment>
           ))}
-          </span>                                  
+        </span>                                 
         </div>      
     </header>
   );
