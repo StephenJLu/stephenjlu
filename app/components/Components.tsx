955192e7e75c326892b5237389a8c324d36157e0
Stephen J. Lu <Stephen@StephenJLu.com>
2024-12-13 09:13:20 -0800

    Refactor theme provider exports and remove obsolete index file

--- app/components/Components.tsx
+++ app/components/Components.tsx
@@ -4,4 +4,6 @@ export { default as TextFade } from './text-animation/TextFade';
 export { DecoderText } from './decoder-text/decoder-text';
 export { default as InViewport } from './in-viewport/InViewport';
 export { default as MenuButton } from './button/MenuButton';
+export { ThemeProvider, themeStyles, ThemeContext, useTheme } from './theme-provider/theme-provider';
+export { themes } from './theme-provider/theme';
 export { Icon } from './icon';
