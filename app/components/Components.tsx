2d8dfda0d7ff0306965b6a9ad7fde7d29fe061ec
Stephen J. Lu <Stephen@StephenJLu.com>
2024-12-12 20:21:59 -0800

    Add LinkedIn icon and update workspace configuration

--- app/components/Components.tsx
+++ app/components/Components.tsx
@@ -4,6 +4,4 @@ export { default as TextFade } from './text-animation/TextFade';
 export { DecoderText } from './decoder-text/decoder-text';
 export { default as InViewport } from './in-viewport/InViewport';
 export { default as MenuButton } from './button/MenuButton';
-export { ThemeProvider, themeStyles, ThemeContext, useTheme }
-     from './theme-provider/theme-provider';
-export { tokens } from './theme-provider/theme';
+export { Icon } from './icon';
