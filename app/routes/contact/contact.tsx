38239971a1d3705d1125552a3b729110eeb9c6ee
Stephen J. Lu <Stephen@StephenJLu.com>
2024-12-17 06:19:45 -0800

    Refined page transitions

--- app/routes/contact/contact.tsx
+++ app/routes/contact/contact.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Section, Heading, Text, Link } from '../../components/Components';
+import React, { useState } from 'react';
+import { Section, Heading, Text, Link, InViewport } from '../../components/Components';
 import { baseMeta } from '../../utils/meta';
 import styles from './contact.module.css';
 
@@ -17,8 +17,8 @@ export const meta = () => {
   });
 };
 
-export const Contact: React.FC<ContactProps> = ({ id, sectionRef, scrollIndicatorHidden, ...rest }) => {  
-  const theme = 'dark';
+export const Contact: React.FC<ContactProps> = ({ id, sectionRef, scrollIndicatorHidden, ...rest }) => {    
+  const [hasEnteredViewport, setHasEnteredViewport] = useState(false);
   const titleId = `${id}-title`;
 
   
@@ -33,13 +33,24 @@ export const Contact: React.FC<ContactProps> = ({ id, sectionRef, scrollIndicato
       tabIndex={-1}
       {...rest}
       >
-       <Heading level={1} as={'h1'}>Contact Me</Heading>
-                 <Heading level={2} as={'h2'}>This new website is still under construction.</Heading>                   
-             <Text as={'p'}>              
-                 I'm currently converting my legacy website to a new, modern, and responsive design, based on <Link href="https://react.dev/">React</Link>. I know it's probably overkill for a personal website/portfolio, but I learn best by screwing up. Some things might look screwy on your browser or mobile right now.
-                 I'm working on it. You can check out the <Link href="https://storybook.stephenjlu.com/">Storybook</Link> to see the component designs.<br /><br />
-                 In the meantime, you can find me at my <Link href="https://legacy.StephenJLu.com/">legacy website</Link> or on <Link href="https://www.linkedin.com/in/stephenjlu/">LinkedIn</Link>.        
-                  </Text> 
+       <InViewport>
+        {(isInViewport) => {
+          if (isInViewport && !hasEnteredViewport) {
+            setHasEnteredViewport(true);
+          }
+          return (
+            <div className={hasEnteredViewport ? styles.inView : styles.notInView}>              
+              <Heading level={1} as={'h1'}>Contact Me</Heading>
+              <Heading level={2} as={'h2'}>This new website is still under construction.</Heading>
+              <Text as={'p'}>
+                I&apos;m currently converting my legacy website to a new, modern, and responsive design, based on <Link href="https://react.dev/">React</Link>. I know it&apos;s probably overkill for a personal website/portfolio, but I learn best by screwing up. Some things might look screwy on your browser or mobile right now.
+                I&apos;m working on it. You can check out the <Link href="https://storybook.stephenjlu.com/">Storybook</Link> to see the component designs.<br /><br />
+                In the meantime, you can find me at my <Link href="https://legacy.StephenJLu.com/">legacy website</Link> or on <Link href="https://www.linkedin.com/in/stephenjlu/">LinkedIn</Link>.
+              </Text>
+            </div>
+          );
+        }}
+      </InViewport>     
     </Section>
        </div>
   );
