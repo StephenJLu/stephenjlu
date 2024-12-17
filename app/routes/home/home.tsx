7dc2f36b6ae2e93fc100309b7b69915692f1c1e8
Stephen J. Lu <Stephen@StephenJLu.com>
2024-12-17 07:39:51 -0800

    updated polaroid on home page for random rotation

--- app/routes/home/home.tsx
+++ app/routes/home/home.tsx
@@ -32,10 +32,7 @@ export const Home: React.FC<HomeProps> = ({ id, sectionRef, scrollIndicatorHidde
           return (
             <div className={hasEnteredViewport ? styles.inView : styles.notInView}>
               <div className={styles.polaroid}>
-                <PolaroidImage
-                  imageUrl={imageUrl}
-                  rotation={-12}
-                />
+                <PolaroidImage imageUrl={imageUrl} />
               </div>
               <Heading level={1} as={'h1'}>Hi!</Heading>
               <Heading level={2} as={'h2'}>This new website is still under construction.</Heading>
