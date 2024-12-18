797e041cd7d6bc706043784d7e6ecaa54cba0d6a
Stephen J. Lu <Stephen@StephenJLu.com>
2024-12-18 13:26:10 -0800

    Intro layouts

--- app/routes/home/home.tsx
+++ app/routes/home/home.tsx
@@ -17,19 +17,19 @@ interface HomeComponentProps extends HomeProps {
 const HomeText = ({ visible, titleId }: { visible: boolean; titleId: string }) => (
   <>
     <Heading className={styles.title} data-visible={visible} level={3} weight={'light'} id={titleId}>Hello there</Heading>
-    <Text className={styles.description} data-visible={visible} size="l" as="p">
+    <Text className={styles.description} data-visible={visible} size="l" as={'p'}>
       I'm a retired Crime Scene Investigator and Forensic Firearms Examiner-turned-front-end web designer and developer. Throughout
       my varied careers, I've studied everything from mosquitoes and disease biology to bloodstain patterns,
       bullet trajectories, and digging up clandestine graves. I've also worked as a freelance web designer,
       providing services to non-profit organizations and small businesses.
       </Text>
-    <Text className={styles.description} data-visible={visible} size="l" as="p">
+    <Text className={styles.description} data-visible={visible} size="l" as={'p'}>
       I'm currently working on this portfolio website, so please check back soon for updates and changes.
      </Text>
-    <Text className={styles.description} data-visible={visible} size="l" as="p">
+    <Text className={styles.description} data-visible={visible} size="l" as={'p'}>
      In the meantime, you can find more detailed information about me at my <Link href="https://legacy.StephenJLu.com/">legacy website</Link> or on <Link href="https://www.linkedin.com/in/stephenjlu/">LinkedIn</Link>.
      </Text>
-    <Text className={styles.description} data-visible={visible} size="l" as="p" style={{ fontStyle: 'italic' }}>
+    <Text className={styles.description} data-visible={visible} size="l" as={'p'} style={{ fontStyle: 'italic' }}>
      Thanks for visiting!
     </Text>
   </>
@@ -56,7 +56,8 @@ export const Home = ({ id, visible, sectionRef }: HomeComponentProps) => {
       >
         <Transition in={visible || focused} timeout={0} unmount={false}>
           {({ visible, nodeRef }: { visible: boolean; nodeRef: React.RefObject<HTMLDivElement> }) => (
-            <div ref={nodeRef}>
+          <div className={styles.content} ref={nodeRef}>
+            <div className={styles.column}>
               <div className={styles.tag} aria-hidden>
                 <Divider
                   notchWidth="64px"
@@ -82,12 +83,15 @@ export const Home = ({ id, visible, sectionRef }: HomeComponentProps) => {
                     </div>
                   );
                 }}
-              </InViewport>
-            </div>
-            <HomeText visible={visible} titleId={titleId} />          
-          <div className={styles.polaroidContainer} data-visible={visible}>
-                <PolaroidImage rotation={10} imageUrl={steveImage} />
+                </InViewport>            
               </div>
+                <HomeText visible={visible} titleId={titleId} />
+            </div>
+              <div className={styles.column}>                        
+                  <div className={styles.polaroidContainer} data-visible={visible}>
+                    <PolaroidImage rotation={10} imageUrl={steveImage} />
+                  </div>
+              </div>            
           </div>
         )}
       </Transition>
