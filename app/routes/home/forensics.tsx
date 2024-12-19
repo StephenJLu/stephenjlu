da198f45678e9c9547f71f6b47ba5e4dd96713ed
Stephen J. Lu <Stephen@StephenJLu.com>
2024-12-18 20:31:42 -0800

    Final Forensics Section

--- app/routes/home/forensics.tsx
+++ app/routes/home/forensics.tsx
@@ -16,7 +16,7 @@ interface ForensicsComponentProps extends ForensicsProps {
 const ForensicsText = ({ visible, titleId }: { visible: boolean; titleId: string }) => (
   <>  
     <Heading className={styles.title} data-visible={visible} level={3} weight={'light'} id={titleId}>Crime Scene Investigator and Firearms Examiner</Heading>
-    <Table className={styles.table}>
+    <Table data-visible={visible}>
   <TableRow>
     <TableCell>
       <Heading className={styles.title} data-visible={visible} level={4} weight={'regular'}>
@@ -74,7 +74,14 @@ const ForensicsText = ({ visible, titleId }: { visible: boolean; titleId: string
     </TableCell>
   </TableRow>
 </Table>
-      <Heading className={styles.title} data-visible={visible} level={4} weight={'regular'}>Detailed History</Heading>
+      <Heading className={styles.title} data-visible={visible} level={4} weight={'regular'}><br />Detailed Professional History</Heading>
+      <Text className={styles.description} data-visible={visible} size={'l'} as={'p'}>Throughout his forensic career, Stephen has analyzed over a thousand cases and participated in hundreds of death investigations, including homicides, suicides, officer-involved shootings, autopsies, and custodial deaths. Most recently, he served for ten years as a Criminalist with the San Diego County Sheriff’s Department, specializing in Forensic Biology, Forensic Firearms Analysis, and Crime Scene Investigation and Reconstruction.</Text>
+<Text className={styles.description} data-visible={visible} size={'l'} as={'p'}>
+Stephen also spent six years as a contract assessor and trainer with the National Forensic Science Technology Center (now FIU Global Forensic and Justice Center), where he performed DNA laboratory audits and taught courses on DNA amplification, likelihood ratios, and population statistics. Prior to that, Stephen worked with the Arizona Department of Public Safety, performing casework in Forensic Biology, and the California Department of Justice’s Richmond DNA Lab, where he contributed to the FBI’s Combined DNA Index System (CODIS).</Text>
+<Text className={styles.description} data-visible={visible} size={'l'} as={'p'}>
+Over his career, Stephen has completed more than 2,000 hours of professional training in areas such as Forensic Biology, Forensic Firearms Analysis, Trajectory Analysis, Bloodstain Pattern Analysis, and courtroom testimony. He has testified as an expert witness in superior courts in Arizona and California, and in federal court for the U.S. District Court for the Central District of California. Stephen's courtroom experience has been noted for his ability to explain complex scientific concepts in an understandable and engaging way for juries and attorneys alike.</Text>
+<Text className={styles.description} data-visible={visible} size={'l'} as={'p'}>
+In addition to his forensic science work, Stephen served as the Regional Director South for the California Association of Criminalists (CAC), where he organized regional study groups and hosted presentations by experts, including a keynote address by Jeff Udvarhelyi, an Escondido Police Department Child Abuse Detective, on a significant child abuse case. As the Lead Webmaster for the CAC, he enhanced the organization’s public presence by overhauling its website for better communication and engagement.</Text>
   </>
 
 );
