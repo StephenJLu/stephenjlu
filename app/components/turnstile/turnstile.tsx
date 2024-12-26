0b4c6849532988f4e42e77176e874ed50d6703b8
Stephen J. Lu <Stephen@StephenJLu.com>
2024-12-25 20:26:40 -0800

    success required prop

--- app/components/turnstile/turnstile.tsx
+++ app/components/turnstile/turnstile.tsx
@@ -20,7 +20,7 @@ type TurnstileTheme = 'light' | 'dark' | 'auto';
 interface TurnstileProps {  
   className?: string;
   onWidgetId?: (id: string) => void;
-  success?: boolean;
+  success: boolean;
   theme?: TurnstileTheme;  
   [key: string]: any; // Allow additional props
 }
