c02095abb4bad61bd296cca365a28186dbfef88f
Stephen J. Lu <Stephen@StephenJLu.com>
2025-06-04 19:30:01 -0700

    Added Answer ALS

--- app/routes/als/ContentText.tsx
+++ app/routes/als/ContentText.tsx
@@ -11,6 +11,7 @@ const ContentText = ({ visible, titleId }: { visible: boolean; titleId: string }
     <ListItem><Text className={styles.description} data-visible={visible} size={'l'}><Link secondary href={'https://www.als.org/'}>The ALS Association</Link></Text></ListItem>
     <ListItem><Text className={styles.description} data-visible={visible} size={'l'}><Link secondary href={'https://www.alssandiego.org'}>ALS San Diego</Link></Text></ListItem>    
     <ListItem><Text className={styles.description} data-visible={visible} size={'l'}><Link secondary href={'https://www.als.net/'}>ALS Therapy Development Institute</Link></Text></ListItem>
+    <ListItem><Text className={styles.description} data-visible={visible} size={'l'}><Link secondary href={'https://www.answerals.org/'}>Answer ALS</Link></Text></ListItem>
     <ListItem><Text className={styles.description} data-visible={visible} size={'l'}><Link secondary href={'https://augiesquest.org/'}>Augie's Quest</Link></Text></ListItem>
     <ListItem><Text className={styles.description} data-visible={visible} size={'l'}><Link secondary href={'https://axeals.org/'}>axeALS Foundation (Stevens Nation)</Link></Text></ListItem>
     <ListItem><Text className={styles.description} data-visible={visible} size={'l'}><Link secondary href={'https://heralsstory.org/'}>Her ALS Story</Link></Text></ListItem>
