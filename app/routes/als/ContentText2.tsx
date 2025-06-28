b506ac00021f175a46ecffc051c393c01b8ef071
Stephen J. Lu <Stephen@StephenJLu.com>
2025-06-27 18:36:04 -0700

    Updated names

--- app/routes/als/ContentText2.tsx
+++ app/routes/als/ContentText2.tsx
@@ -9,6 +9,7 @@ const ContentText2 = ({ visible, titleId }: { visible: boolean; titleId: string 
   <List className={styles.description} data-visible={visible} size="l" as={'ul'}>    
     <ListItem><Text className={styles.description} data-visible={visible} size={'l'}><Link secondary href={'https://www.instagram.com/carbajalphoto/'}>Anthony Carbajal</Link></Text></ListItem>
     <ListItem><Text className={styles.description} data-visible={visible} size={'l'}><Link secondary href={'https://www.instagram.com/alswarriorjenn/'}>Jennifer Schroedl</Link></Text></ListItem>
+    <ListItem><Text className={styles.description} data-visible={visible} size={'l'}><Link secondary href={'https://www.instagram.com/jfarrell57/'}>John Farrell</Link></Text></ListItem>
     <ListItem><Text className={styles.description} data-visible={visible} size={'l'}><Link secondary href={'https://www.teamhighoctane.com/'}>Julian Rodriguez</Link></Text></ListItem>
     <ListItem><Text className={styles.description} data-visible={visible} size={'l'}><Link secondary href={'https://www.amazon.com/dp/B0DNN4337V'}>Mayuri Saxena</Link></Text></ListItem>
     <ListItem><Text className={styles.description} data-visible={visible} size={'l'}><Link secondary href={'https://sunnystrong.com/'}>Sunny Brous</Link></Text></ListItem>
